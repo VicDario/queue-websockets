c23e6e857400609f75bce567de68c61165c05975
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-29 14:12:55 -0300

    feat: show last ticket and generate new one

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,21 @@
+const currentTicketLabel = document.querySelector('span');
+const createTicketButton = document.querySelector('button');
+
+async function getLastTicket() {
+  const response = await fetch('/api/tickets/last');
+  const lastTicket = await response.json();
+  currentTicketLabel.innerText = lastTicket;
+}
 
 
-console.log('Nuevo Ticket HTML');
+async function createTicket() {
+  const response = await fetch('/api/tickets', {
+    method: 'POST'
+  });
+  const newTicket = await response.json();
+  currentTicketLabel.innerText = newTicket.number;
+}
+
+createTicketButton.addEventListener('click', createTicket);
+
+getLastTicket();
