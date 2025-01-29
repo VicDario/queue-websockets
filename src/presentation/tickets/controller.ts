9f83aca2481afe1b58d1bf2d61e90b073440c5cc
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-29 13:47:08 -0300

    feat: use service in controller

--- src/presentation/tickets/controller.ts
+++ src/presentation/tickets/controller.ts
@@ -1,34 +1,37 @@
 import { Request, Response } from "express";
+import { TicketService } from "../services/ticket.service";
 
 export class TicketController {
-  constructor() {}
+  constructor(private readonly ticketService: TicketService = new TicketService()) {}
 
-  public getTickets = async (req: Request, res: Response) => {
-    res.json('getTickets');
+  public getTickets = async (_req: Request, res: Response) => {
+    res.json(this.ticketService.tickets);
   }
 
-  public getLastTicketNumber = async (req: Request, res: Response) => {
-    res.json('getTickets');
+  public getLastTicketNumber = async (_req: Request, res: Response) => {
+    res.json(this.ticketService.lastTicketNumber);
   }
 
-  public pendingTickets = async (req: Request, res: Response) => {
-    res.json('pendingTickets');
+  public pendingTickets = async (_req: Request, res: Response) => {
+    res.json(this.ticketService.pendingTickets);
   }
 
-  public createTickets = async (req: Request, res: Response) => {
-    res.json('createTickets');
+  public createTickets = async (_req: Request, res: Response) => {
+    res.status(201).json(this.ticketService.createTicket());
   }
 
   public drawTickets = async (req: Request, res: Response) => {
-    res.json('drawTickets');
+    const { desk } = req.params;
+    res.json(this.ticketService.drawTicket(desk));
   }
 
   public ticketFinished = async (req: Request, res: Response) => {
-    res.json('ticketFinished');
+    const { ticketId } = req.params;
+    res.json(this.ticketService.onFinishedTicket(ticketId));
   }
 
-  public workingOn = async (req: Request, res: Response) => {
-    res.json('workingOn');
+  public workingOn = async (_req: Request, res: Response) => {
+    res.json(this.ticketService.lastWorkingOnTickets);
   }
 
 }
