ec9ea844212a3c9f064c32390cad00b5f04a22b0
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-30 13:33:32 -0300

    feat: update last ticket number on event

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,3 +1,50 @@
+const deskHeader = document.querySelector('h1');
+const lblPending = document.querySelector('#lbl-pending');
+const noMoreAlert = document.querySelector('.alert');
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html';
+  throw new Error('Desk is required');
+}
+const deskNumber = searchParams.get('escritorio');
+
+deskHeader.innerText = deskNumber;
+
+function checkTicketCount(initialCount = 0) {
+  if (initialCount === 0) noMoreAlert.classList.remove('d-none');
+  else noMoreAlert.classList.add('d-none');
+  lblPending.innerHTML = initialCount;
+}
+
+async function loadInitialCount() {
+  const response = await fetch('/api/tickets/pending');
+  const pendingTickets = await response.json();
+  checkTicketCount(pendingTickets.length);
+}
+
+function connectToWebSockets() {
+  const socket = new WebSocket('ws://localhost:3000/ws');
+
+  socket.onmessage = (event) => {
+    const { type, payload } = JSON.parse(event.data);
+    if (type !== 'on-ticket-number-changed') return;
+    checkTicketCount(payload);
+  };
+
+  socket.onclose = (event) => {
+    console.log('Connection closed');
+    setTimeout(() => {
+      console.log('retrying to connect');
+      connectToWebSockets();
+    }, 1500);
+  };
+
+  socket.onopen = (event) => {
+    console.log('Connected');
+  };
+}
+
+connectToWebSockets();
+loadInitialCount();
